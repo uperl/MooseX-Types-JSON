a445ea4a4de533dc312b908c6c30455fe6a14325
Graham Ollis <plicease@cpan.org>
2021-12-16 17:37:00 -0700

    add relaxedJSON to import in synopsis

--- lib/MooseX/Types/JSON.pm
+++ lib/MooseX/Types/JSON.pm
@@ -18,7 +18,7 @@ MooseX::Types::JSON - JSON datatype for Moose
 
  use Moose;
  use Moose::Util::TypeConstraints;
- use MooseX::Types::JSON qw( JSON );
+ use MooseX::Types::JSON qw( JSON relaxedJSON );
 
  has config  => ( is => 'rw', isa => JSON        );
  has options => ( is => 'rw', isa => relaxedJSON );
